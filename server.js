b2ab37badc8e7deb22d71730ce65ece644cc71d9
Fabien Grignoux <fabien.grignoux@outlook.com>
2025-02-14 09:20:09 +0100

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 require("dotenv").config();
 
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const API_KEY = process.env.GETRESPONSE_API_KEY;
